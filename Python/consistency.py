cbaa7fc51f5d9c9f9354f54c0c85d0aca4396747
Flavio Monteiro <flaviomonteiro2013@gmail.com>
2017-01-30 19:57:22 -0200

    Apply autopep8 to Python/consistency.py

--- Python/consistency.py
+++ Python/consistency.py
@@ -11,7 +11,8 @@
 #  * (pip package?)                          #
 #--------------------------------------------#
 
-import glob, os
+import glob
+import os
 
 """
   This function changes all the detected files within a directory, using
@@ -21,20 +22,22 @@ their trailing tabs and spaces stripped.
   It also ensures that those files EOL character is the one specified in
 line 31 and 32, and that the file will end with an EOL character.
 """
+
+
 def refactor(dir):
-	os.chdir((dir+"/"))
-	for file in glob.glob("*.py"): # Change to match your requirements
-		f = open(file, 'r+')
+    os.chdir((dir + "/"))
+    for file in glob.glob("*.py"):  # Change to match your requirements
+        f = open(file, 'r+')
 
-		lines = f.readlines()
-		lines = [line.rstrip() for line in lines]
-		text = "\r\n".join(lines) # Change to match your desired EOL
-		text += "\r\n"            # Same as above
+        lines = f.readlines()
+        lines = [line.rstrip() for line in lines]
+        text = "\r\n".join(lines)  # Change to match your desired EOL
+        text += "\r\n"            # Same as above
 
-		f.seek(0)
-		f.write(text)
-		f.truncate()
-		f.close()
+        f.seek(0)
+        f.write(text)
+        f.truncate()
+        f.close()
 
 #
 #   If for example, one wants to refactor 'folder1' and 'folder2', which
